69e19b21bbaf08bcabe494ebed54aef2b5b82d4f
EliotBordier <eliot.bordier@gmail.com>
2024-12-02 20:47:52 +0100

    Launcher Update
    Added dynamic launcher

---
+++ Platformer/launcher.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import subprocess
+from pathlib import Path
+from PIL import Image, ImageTk
+from itertools import count, cycle
+
+class ImageLabel(tk.Label):
+    """Un Label qui affiche des images et les joue si ce sont des GIFs."""
+    def load(self, im):
+        if isinstance(im, str):
+            im = Image.open(im)
+        frames = []
+        try:
+            for i in count(1):
+                frames.append(ImageTk.PhotoImage(im.copy()))
+                im.seek(i)
+        except EOFError:
+            pass
+        self.frames = cycle(frames)
+        try:
+            self.delay = im.info['duration']
+        except:
+            self.delay = 100
+        if len(frames) == 1:
+            self.config(image=next(self.frames))
+        else:
+            self.next_frame()
+
+    def unload(self):
+        self.config(image=None)
+        self.frames = None
+
+    def next_frame(self):
+        if self.frames:
+            self.config(image=next(self.frames))
+            self.after(self.delay, self.next_frame)
+
+root = tk.Tk()
+root.title("Launcher")
+root.geometry("1000x373")
+root.resizable(False, False)
+
+# Conteneur principal
+container = tk.Frame(root, width=600, height=300)
+container.pack(fill='both', expand=True)
+
+lbl = ImageLabel(container)
+lbl.place(relx=0.5, rely=0.5, anchor='center')  # Centré dans le conteneur
+lbl.load(r'Python\NSI\Platformer V2\Launcher.gif')
+
+label_title = tk.Label(container, text="Platformer", font=('Papyrus', 30, 'bold'), bg="grey", fg="black")
+label_title.place(relx=0.5, rely=0.2, anchor='center')
+
+# Chemin du programme Python à exécuter
+chemin_platformer = Path(r"Python\NSI\Platformer V2\main.py").resolve()
+
+# Exécuter le programme
+def run_program():
+    # Femre la fenêtre principale
+    root.destroy()
+    # Exécuter le programme
+    subprocess.run(["python", chemin_platformer])
+
+# Créer le bouton
+button_run = tk.Button(container, text="Lancer le jeu", command=run_program, font=('Arial', 12, 'bold'), bg="blue", fg="white", cursor="hand2")  
+button_run.place(relx=0.5, rely=0.7, anchor='center')  # Position centré un peu plus bas que le centre
+
+root.mainloop()
