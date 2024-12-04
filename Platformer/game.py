129abdb1cbb8d3fab183d1a4866a6cf3b2a33831
EliotBordier <eliot.bordier@gmail.com>
2024-12-04 18:55:35 +0100

    Added a gif to the game backscreen

--- Platformer/game.py
+++ Platformer/game.py
@@ -4,14 +4,20 @@ import subprocess
 from player import Player
 from constantes import Constantes
 from platforms import PlatformsManager
+from load_backscreen import load_backsceen
 
 class Game:
     def __init__(self):
         self.player = Player()
         self.const = Constantes()
         self.platform_manager = PlatformsManager()
+        self.bkscreen = load_backsceen()
         self.clock = self.const.CLOCK
         self.running = True
+
+        # Charger les frames du GIF
+        self.frames = self.bkscreen.load_gif(r'.\Clouds.gif')
+        self.frame_index = 0
         #self.gameover_file = sys.path.append('/platformer/gameover.py')
 
     def run(self):
@@ -26,7 +32,9 @@ class Game:
             self.player.update(self.platform_manager.platforms)
 
             # Dessin
-            self.const.SCREEN.fill(self.const.WHITE)
+            self.const.SCREEN.fill((0, 0, 0))
+            self.const.SCREEN.blit(self.frames[self.frame_index], (0, 0))
+            self.frame_index = (self.frame_index + 1) % len(self.frames)
             self.platform_manager.draw_platforms()
             self.player.draw()
 
